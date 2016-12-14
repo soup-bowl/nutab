359fbd8d8e0ff93149877bf3dba12bcbcfa475bf
soup-bowl <soup-bowl@users.noreply.github.com>
2016-12-14 21:52:36 +0000

    Bookmark code clean-up.

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -4,36 +4,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     displayBookmarks();
 });
 
+/**
+ * Populate the bookmarks bar with favourites.
+ */
 function displayBookmarks() {
     // "1" being the Bookmarks Bar, grab the bookmark tree.
     bmk.getSubTree("1", function(e) {
 
         var bookmarkBar = document.getElementById("bookmarksbar");
 
-        // Iterate through the bookmark nodes.
-        e[0].children.forEach(function(bookmark) {
-            // TODO - Support folders. For now, skip them.
-            if (bookmark.children !== undefined)
-                return;
-            
-            var bookmarkEntry = document.createElement("LI"); // Container
-            var bookmarkLink = document.createElement("A");   // Hyperlink
-            var bookmarkIcon = document.createElement("IMG"); // Favicon
-            
-            bookmarkLink.setAttribute("href", bookmark.url);
-            bookmarkIcon.setAttribute("src", "chrome://favicon/" + bookmark.url);
-            bookmarkIcon.setAttribute("class", "favicon");
-            bookmarkIcon.setAttribute("title", bookmark.title);
-
-            if (bookmark.url === "chrome://apps/") {
-                bookmarkIcon.setAttribute("id", "applauncher");
-                bookmarkIcon.setAttribute("title", "App Launcher");
-            }
-            
-            bookmarkLink.appendChild(bookmarkIcon);
-            bookmarkEntry.appendChild(bookmarkLink);
-            bookmarkBar.appendChild(bookmarkEntry);
-        });
+        createBookmarkIcons(bookmarkBar, e[0].children);
 
         // Enable launching of the App tray, which is otherwise blocked.
         document.getElementById("applauncher").addEventListener("click", function() { 
@@ -41,4 +21,41 @@ function displayBookmarks() {
             chrome.tabs.update({url: "chrome://apps/"});
         });
     }); 
+}
+
+/**
+ * Creates a HTML image with hyperlink representing the favicon, and hyperlink to the bookmark.
+ * @param {Object}           parent   - UL/OL DOM where the bookmark bar is located. 
+ * @param {BookmarkTreeNode} bookmark 
+ */
+function createBookmarkIcon(parent, bookmark) {
+    var bookmarkEntry = document.createElement("LI");
+    var bookmarkLink = document.createElement("A");   // Hyperlink
+    var bookmarkIcon = document.createElement("IMG"); // Favicon
+    
+    bookmarkLink.setAttribute("href", bookmark.url);
+    bookmarkIcon.setAttribute("src", "chrome://favicon/" + bookmark.url);
+    bookmarkIcon.setAttribute("class", "favicon");
+    bookmarkIcon.setAttribute("title", bookmark.title);
+
+    if (bookmark.url === "chrome://apps/") {
+        bookmarkIcon.setAttribute("id", "applauncher");
+        bookmarkIcon.setAttribute("title", "App Launcher");
+    }
+    
+    bookmarkLink.appendChild(bookmarkIcon);
+    bookmarkEntry.appendChild(bookmarkLink);
+    parent.appendChild(bookmarkEntry);
+}
+
+/** 
+ * Creates a collection of HTML images with hyperlinks representing the respective favicon, and hyperlink to the said bookmark.
+ * @see {@link createBookmarkIcon}
+ * @param {Object}           parent    - UL/OL DOM where the bookmark bar is located. 
+ * @param {BookmarkTreeNode} bookmarks - Collection of BookmarkTreeNode.
+ */
+function createBookmarkIcons(parent, bookmarks) {
+    bookmarks.forEach(function(bookmark) {
+        createBookmarkIcon(parent, bookmark);
+    });
 }
